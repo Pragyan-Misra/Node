d2ddc989185a011607d6571bf5a0017ce72c8fbc
Pragyan Misra <pragyanmisra080625@gmail.com>
2026-01-16 15:48:58 +0530

    Express

--- Server_Express/index.js
+++ Server_Express/index.js
@@ -1,31 +1,20 @@
 const http = require("http");
-const fs =require("fs");
-const url = require("url");
+const express = require("express");
 
-const myServer = http.createServer( (req, res) => {
-  if (req.url === "/favicon.ico") res.end();
+const app = express();
 
-  const log = `${Date.now()}: ${req.method} ${req.url} New Req Rec.\n`;
-  const myurl = url.parse(req.url, true);
-  console.log(myurl)
-  fs.appendFile("log.txt", log, (err, data) => {
-    switch(myurl.pathname){
-      case '/': res.end("HomePage");
-      break;
-      case '/about':
-        const username = myurl.query.myname;
-        res.end(`I am ${username}`);
-        break;
-      case '/contact':
-        const contact = myurl.query.userType
-        res.end(`Here is ${contact} available Contact :+912344556`);
-        break;
-      default:
-        res.end("404 Not Found");
-    }
-  })
-} );
-
-myServer.listen(8000, () => {
-  console.log("Server Started")
+app.get('/', (req, res) => {
+  return res.send("Hello, from Home Page");
 });
+
+app.get('/about', (req, res) => {
+  return res.send(`Hello ${req.query.name}`);
+});
+
+app.listen( 8000, () => console.log("Server Started"));
+
+// const myServer = http.createServer(app);
+
+// myServer.listen(8000, () => {
+//   console.log("Server Started")
+// });
---
+++ Server_Express/indexServer.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const fs =require("fs");
+const url = require("url");
+
+const myServer = http.createServer( (req, res) => {
+  if (req.url === "/favicon.ico") res.end();
+
+  const log = `${Date.now()}: ${req.method} ${req.url} New Req Rec.\n`;
+  const myurl = url.parse(req.url, true);
+  console.log(myurl)
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch(myurl.pathname){
+      case '/': res.end("HomePage");
+      break;
+      case '/about':
+        const username = myurl.query.myname;
+        res.end(`I am ${username}`);
+        break;
+      case '/contact':
+        const contact = myurl.query.userType
+        res.end(`Here is ${contact} available Contact :+912344556`);
+        break;
+      default:
+        res.end("404 Not Found");
+    }
+  })
+} );
+
+myServer.listen(8000, () => {
+  console.log("Server Started")
+});
